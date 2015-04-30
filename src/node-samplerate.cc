b0fd106b688462fea147e176cb8d285d6a03178f
Frederick Gnodtke <frederick@gnodtke.net>
2015-04-30 15:16:52 +0200

    More readable code.

--- src/node-samplerate.cc
+++ src/node-samplerate.cc
@@ -1,6 +1,7 @@
 #include <v8.h>
 #include <nan.h>
 #include <node_buffer.h>
+#include <iostream>
 
 #include "../deps/libsamplerate/src/samplerate.h"
 
@@ -24,6 +25,9 @@ static NAN_METHOD(Resample) {
 
 	long inputFrames = (long) args[4]->NumberValue(); // Amount of frames to process
 
+	double ratio = toRatio / (double) fromRatio; // Ratio calculated by dividing output-ratio by input-ratio
+	std::cout << "Ratio:" << ratio << std::endl;
+
 	unsigned int lengthIn = Buffer::Length(inputBuffer); //Length of the input data
 	unsigned int lengthOut = lengthIn * ratio; //Length of the outputdata //TODO: How to calculate this value?
 
@@ -32,10 +36,10 @@ static NAN_METHOD(Resample) {
 	float *dataOutFloat = new float[lengthOut]; //Buffer for outputdata, as float array
 	float *dataInFloat = new float[lengthOut]; //Buffer for inputdata, as float array
 
-	double ratio = toRatio / (double) fromRatio; // Ratio calculated by dividing output-ratio by input-ratio
+	src_short_to_float_array(dataIn, dataInFloat, lengthIn); //Convert the input from short array to float array
 
-	src_short_to_float_array(dataIn, dataInFloat, lengthIn);
-	outputFrames = ratio * inputFrames + 1;
+	long outputFrames = ratio * inputFrames + 1;
+
 	SRC_DATA data;
 		data.data_in = dataInFloat;
 		data.data_out = dataOutFloat;
@@ -43,10 +47,13 @@ static NAN_METHOD(Resample) {
 		data.output_frames = outputFrames;
 		data.src_ratio = ratio;
 	src_simple (&data, SRC_SINC_FASTEST, channels);
-	long outputFrames = data.output_frames_gen;
+
+	outputFrames = data.output_frames_gen; //Get actual values for frames
 	inputFrames = data.input_frames_used;
-	src_float_to_short_array(dataOutFloat, dataOut, lengthOut);
-	Local<Object> outputBuffer = NanNewBufferHandle(reinterpret_cast<char*>(dataOut), lengthOut);
+
+	src_float_to_short_array(dataOutFloat, dataOut, lengthOut);  //Convert the output from float array to short array
+
+	Local<Object> outputBuffer = NanNewBufferHandle(reinterpret_cast<char*>(dataOut), lengthOut); //Return the result
 	NanReturnValue(outputBuffer);
 }
 
