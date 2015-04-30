2181e1940921caecfd89560a8680f9726b1e828e
Frederick Gnodtke <frederick@gnodtke.net>
2015-04-30 16:59:21 +0200

    Fixed samplerate.

--- src/node-samplerate.cc
+++ src/node-samplerate.cc
@@ -28,28 +28,29 @@ static NAN_METHOD(Resample) {
 
 	double ratio = toRatio / (double) fromRatio; // Ratio calculated by dividing output-ratio by input-ratio
 
-	unsigned int lengthIn = Buffer::Length(inputBuffer); //Length of the input data
-	unsigned int lengthOut = lengthIn * ratio; //Length of the outputdata //TODO: How to calculate this value?
+	unsigned int lengthIn = Buffer::Length(inputBuffer) / sizeof(short); //Length of the input data
+	unsigned int lengthOut = lengthIn * ratio + 1; //Length of the outputdata
 
 	//long inputFrames = (long) args[4]->NumberValue(); // Amount of frames to process
 	//long inputFrames = Buffer::Length(inputBuffer) / fromRatio;
-	long inputFrames = lengthIn / channels;
-	std::cout << "Input Buffer Length:" << Buffer::Length(inputBuffer) << std::endl;
-	std::cout << "Input Frames:" << inputFrames << std::endl;
+	long inputFrames = (lengthIn / channels); // Divide by 2 as we use 16-bit
+	std::cout << "  Input Buffer Length:" << Buffer::Length(inputBuffer) << std::endl;
+	std::cout << "  Input Frames:" << inputFrames << std::endl;
 
-	std::cout << "Ratio:" << ratio << std::endl;
+	std::cout << "  Ratio:" << ratio << std::endl;
 
 
-	short *dataIn = (short*) Buffer::Data(inputBuffer); //Buffer for inputdata, as short array
+	short *dataIn = reinterpret_cast<short*>(Buffer::Data(inputBuffer)); //Buffer for inputdata, as short array
 	short *dataOut = new short[lengthOut]; //Buffer for outputdata, as short array
 	float *dataOutFloat = new float[lengthOut]; //Buffer for outputdata, as float array
-	float *dataInFloat = new float[lengthOut]; //Buffer for inputdata, as float array
+	float *dataInFloat = new float[lengthIn]; //Buffer for inputdata, as float array
 
 	src_short_to_float_array(dataIn, dataInFloat, lengthIn); //Convert the input from short array to float array
 
-	long outputFrames = 3 * ratio * inputFrames + 1;
+	long outputFrames = ratio * inputFrames + 1;
 	//long outputFrames = 2;
-	std::cout << "Output Frames:" << outputFrames << std::endl;
+	std::cout << "  Output Frames:" << outputFrames << std::endl;
+
 
 	SRC_DATA data;
 		data.data_in = dataInFloat;
@@ -57,14 +58,19 @@ static NAN_METHOD(Resample) {
 		data.input_frames = inputFrames;
 		data.output_frames = outputFrames;
 		data.src_ratio = ratio;
-	src_simple (&data, SRC_SINC_FASTEST, channels);
+	src_simple (&data, SRC_SINC_MEDIUM_QUALITY, channels);
 
 	outputFrames = data.output_frames_gen; //Get actual values for frames
 	inputFrames = data.input_frames_used;
 
+	std::cout << std::endl;
+	std::cout << "  Actual Input Frames:" << inputFrames << std::endl;
+	std::cout << "  Actual Output Frames:" << outputFrames << std::endl;
+	std::cout << std::endl;
+
 	src_float_to_short_array(dataOutFloat, dataOut, lengthOut);  //Convert the output from float array to short array
 
-	Local<Object> outputBuffer = NanNewBufferHandle(reinterpret_cast<char*>(dataOut), lengthOut); //Return the result
+	Local<Object> outputBuffer = NanNewBufferHandle(reinterpret_cast<char*>(dataOut), lengthOut*sizeof(short)); //Return the result
 	NanReturnValue(outputBuffer);
 }
 
